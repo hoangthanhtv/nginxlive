7f7dcae23c3ed7e335eafa05cf620935251ed174
hoangthanhtv <46924315+hoangthanhtv@users.noreply.github.com>
2020-11-02 22:35:24 +0700

    thêm sudo

--- nginx-ubuntu.sh
+++ nginx-ubuntu.sh
@@ -11,8 +11,8 @@ cd nginx-1.19.4
 
 ./configure --user=nginx --group=nginx --add-module=../nginx-rtmp-module/ --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-mail --with-mail_ssl_module --with-file-aio --with-ipv6
 
-make
-make install
-useradd -r nginx
-mkdir -p /var/cache/nginx/client_temp/
-chown nginx. /var/cache/nginx/client_temp/
+sudo make
+sudo make install
+sudo useradd -r nginx
+sudo mkdir -p /var/cache/nginx/client_temp/
+sudo chown nginx. /var/cache/nginx/client_temp/
